6e3d37104d5b93fb8e38c85cea944e5fd2709143
rachanargithub <rachanabala21@gmail.com>
2025-10-18 16:17:31 +0000

    refactor:type hint issue

--- homeassistant/components/assist_pipeline/pipeline.py
+++ homeassistant/components/assist_pipeline/pipeline.py
@@ -551,7 +551,8 @@ class PipelineRun:
     start_stage: PipelineStage
     end_stage: PipelineStage
     event_callback: PipelineEventCallback
-    language: str = None  # type: ignore[assignment]
+    # language: str = None  # type: ignore[assignment]
+    language: str  # this is the fix : type hinting issue
     runner_data: Any | None = None
     intent_agent: conversation.AgentInfo | None = None
     tts_audio_output: str | dict[str, Any] | None = None
